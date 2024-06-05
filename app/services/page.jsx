fa5bd6ba763484d83e4af1572f847ffdf1c827b4
Dmitry-Klyuev <i6169696@gmail.com>
2024-06-05 21:57:16 +0300

    section services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,68 @@
+'use client'
+import Link from 'next/link'
+import {motion} from 'framer-motion'
+import {BsArrowDownRight} from "react-icons/bs";
+
+
+const services = [
+    {
+        num: 1,
+        title: 'Frontend Developer',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consectetur cumque deleniti dolor, doloribus, esse exercitationem explicab',
+        href: ''
+    },
+    {
+        num: 2,
+        title: 'Backend Developer',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consectetur cumque deleniti dolor, doloribus, esse exercitationem explicab',
+        href: ''
+    },
+    {
+        num: 3,
+        title: 'SEO',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consectetur cumque deleniti dolor, doloribus, esse exercitationem explicab',
+        href: ''
+
+    },
+    {
+        num: 4,
+        title: 'UI/UX Design',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consectetur cumque deleniti dolor, doloribus, esse exercitationem explicab',
+        href: ''
+
+    }
+
+]
 
 const Services = () => {
     return (
-        <div>
-            <h2>Services</h2>
-        </div>
+        <section className={'min-h-[80vh] flex flex-col justify-center py-12 xl:py-8'}>
+            <div className={'container mx-auto'}>
+                <motion.div initial={{opacity: 0}}
+                            animate={{
+                                opacity: 1,
+                                transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}
+                            }}
+                            className={'grid grid-cols-1 md:grid-cols-2 gap-[60px]'}
+                >
+                    {services.map(el => {
+                        return <div key={el.num}
+                                    className={'flex flex-1 flex-col justify-center gap-6 group'}
+                            >
+                            <div className={'flex justify-between items-center'}>
+                                <div>{el.num}</div>
+                                <Link href={el.href}>
+                                    <BsArrowDownRight/>
+                                </Link>
+                            </div>
+                            <h2>{el.title}</h2>
+                            <p>{el.description}</p>
+                            <div className={'border-b border-white/20 w-full'}></div>
+                        </div>
+                    })}
+                </motion.div>
+            </div>
+        </section>
     );
 };
 
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -26,16 +26,14 @@ export const Stats = () => {
             <div className="container mx-auto">
                 <div className={'flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none'}>
                     {stats.map((item, index) => {
-                        return <div key={index} className={'flex flex-1 gap-4 items-center justify-center xl:justify-start'}>
-                            <CountUp end={item.num} duration={5} delay={2} className={'text-4xl xl:text-6xl'} />
+                        return <div key={index}
+                                    className={'flex flex-1 gap-4 items-center justify-center xl:justify-start'}>
+                            <CountUp end={item.num} duration={5} delay={2} className={'text-4xl xl:text-6xl'}/>
                             <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
-
                         </div>
                     })}
                 </div>
-
             </div>
-            Stats
         </section>
     );
 };
